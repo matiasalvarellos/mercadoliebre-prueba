feb892049014dabca660ef67f1b7668d15cdb7a9
matiasalvarellos <matias.alvarellos@gmail.com>
2021-07-29 01:58:12 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require ('express')
+const path = require("path")
+const app = express ();
+
+const viewsPath = path.resolve(__dirname,"./views");
+const publiPatch = path.resolve(__dirname, "./public");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(express.static(publiPatch));
+
+
+app.listen (3000, function (){
+  console.log ("La app esta funcionando en la url http://localhost:3000")
+})
+
+app.get ("/",(req, res)=>{
+  return res.sendFile(`${viewsPath}/index.html`)
+})
+
+app.get("/login", (req, res)=>{
+  res.sendFile(`${viewsPath}/login.html`)
+})
+
+app.get("/register", (req, res)=>{
+  res.sendFile(`${viewsPath}/register.html`)
+})
+
+app.get("/prueba", (req, res) => {
+  res.sendFile(`${viewsPath}/prueba.html`)
+})
+
